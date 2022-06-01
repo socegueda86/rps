3c01f72c7eb15a8637c88fa29e54657230e12ef3
socegueda86 <socegueda86@gmail.com>
2022-06-01 17:08:58 -0500

    Create computerPlay Fx

--- script.js
+++ script.js
@@ -9,8 +9,38 @@ function inputValidation (){
             return userInput;
         }
     }
-    
 }
 
-let userInput;
+function computerPlay () {
 
+        switch (Math.floor(Math.random()*3+1)) {
+        case 1 :
+            computerChoice = "rock";
+            break;
+        case 2 :
+            computerChoice = "paper";
+            break;
+        case 3 :
+            computerChoice = "scissors";
+            break;
+    }
+}
+
+function whoWins ( computerChoice, userInput){
+    if (computerChoice === userInput){
+        return "Draw";
+    }
+    else if ((computerChoice == "rock" && userInput == "paper") || (computerChoice == "paper" && userInput == "scissor") || (computerChoice == "scissors" && userInput == "rock")) {
+        return "User Wins";
+    }
+    else {
+        return "computer wins";
+    }        
+}
+
+
+let userInput;
+let computerChoice;
+let userCount;
+let computerCount;
+let drawCount;
